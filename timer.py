cc2b4dbc2656fcc20fa53e2ff263af258b28bf3a
Higor Camelo <higorcamelo@gmail.com>
2023-03-06 14:01:22 -0300

    Notificações adicionadas

--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 import datetime
 import PySimpleGUI as sg
+import chime
 from plyer import notification
-
 
 class Pomodoro:
     def __init__(self):
@@ -10,17 +10,27 @@ class Pomodoro:
             [sg.Push(), sg.Text('0:00:00', font=('Helvetica', 20), key = '-TEMPO-'), sg.Push()],
             [sg.Button('Iniciar'), sg.Button('Pausar'), sg.Button('Reiniciar')]
         ]
-        self.window =  sg.Window('Relógio Pomodoro', layout)
+        self.window = sg.Window('Relógio Pomodoro', layout)
 
-    def att_tempo(self,tempo):
+    def att_tempo(self, tempo):
         self.window['-TEMPO-'].update(tempo)
+
+    def notificacao(self, mensagem):
+        chime.success()
+        notification.notify(
+            title='Pomodoro',
+            message= mensagem,
+            app_icon=None,
+            timeout=10
+        )
+
 
     def timer_pomodoro(self):
         tempo_trab = 25
         tempo_desc = 5
 
         while True:
-            for i in range(tempo_trab * 1, 0, -1):
+            for i in range(tempo_trab * 60, 0, -1):
                 self.att_tempo(str(datetime.timedelta(seconds=i)))
                 event, values = self.window.read(timeout = 1000)
                 if event == 'Reiniciar' or event == sg.WIN_CLOSED:
@@ -30,12 +40,7 @@ class Pomodoro:
                     event, values = self.window.read()
                     while event != 'Iniciar':
                         event, values = self.window.read()
-            self.nofitication.notify(
-                title = 'Pomodoro',
-                message = 'Hora de descansar!',
-                app_icon = None,
-                timeout = 10
-            )
+            self.notificacao('Hora de descansar')
             for i in range(tempo_desc * 1, 0, -1):
                 self.att_tempo(str(datetime.timedelta(seconds=i)))
                 event, values = self.window.read(timeout = 1000)
@@ -46,7 +51,7 @@ class Pomodoro:
                     event, values = self.window.read()
                     while event != 'Iniciar':
                         event, values = self.window.read()
-            sg.popup('Trabalha')
+            self.notificacao('Hora de voltar ao trabalho!')
 
 
     def iniciar(self):
